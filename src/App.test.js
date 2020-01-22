f0f0257d89cabd56823b9a5780321453018bb2a8
Ethan Whaley <51497042+solidlikeplato@users.noreply.github.com>
2020-01-22 10:49:07 -0600

    stubbed out testing app.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let container
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+  ReactDOM.render(<App />, container);
 });
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+it('renders login page when not logged in', () => {
+  // navigate to '/'
+
+  // test if the <LoginPage /> element exists in document
+});
+
+it('renders home page when logged in', () => {
+  // login
+  // navigate to '/'
+
+  // test if the <HomePage /> element exists in document
+});
+
+it('renders a menu bar', () => {
+  //test for <NavBar /> element
+})
+
+it('renders different menu bar when logged in', () => {
+  // login
+
+  //test for different <NavBar /> element
+})
